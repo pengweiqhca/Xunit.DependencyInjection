f2ca76b7609c0bb8e17536d27ca9edc3ae6a2880
彭伟 <pengweiqhca@sina.com>
2020-11-07 20:27:19 +0800

    Integration TestHost

---
+++ Xunit.DependencyInjection.Test/TestServerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Xunit.DependencyInjection.Test
+{
+    public class TestServerTest
+    {
+        private readonly HttpClient _httpClient;
+
+        public TestServerTest(HttpClient httpClient) => _httpClient = httpClient;
+#if NETCOREAPP3_1
+        [Fact]
+        public async Task HttpTest()
+        {
+            using var response = await _httpClient.GetAsync("/").ConfigureAwait(false);
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.True(Math.Abs(long.Parse(await response.Content.ReadAsStringAsync().ConfigureAwait(false)) - DateTimeOffset.Now.ToUnixTimeMilliseconds()) < 20);
+        }
+#endif
+    }
+}
