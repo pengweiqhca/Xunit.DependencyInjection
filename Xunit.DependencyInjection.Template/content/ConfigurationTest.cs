6fb2e621255b5d4c52dcba33d43d36288999617d
weihanli <weihanli@outlook.com>
2020-12-06 14:06:18 +0800

    add custom project template

---
+++ Xunit.DependencyInjection.Template/content/ConfigurationTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace TProject
+{
+    public class ConfigurationTest
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigurationTest(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Fact]
+        public void ConfigurationGetTest()
+        {
+            Assert.NotNull(_configuration);
+        }
+    }
+}
---
+++ Xunit.DependencyInjection.Template/content/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace TProject
+{
+    public class Startup
+    {
+        // create custom hostBuilder with this method
+        // public IHostBuilder CreateHostBuilder()
+        // {
+        // }
+
+        // custom host build
+        public void ConfigureHost(IHostBuilder hostBuilder)
+        {
+            // hostBuilder
+            //     .ConfigureHostConfiguration(builder =>
+            //     {
+            //         builder.AddInMemoryCollection(new Dictionary<string, string>()
+            //             {
+            //                 { "UserName", "Alice" }
+            //             });
+            //     });
+        }
+
+        // add services need to injection
+        // ConfigureServices(IServiceCollection services)
+        // ConfigureServices(IServiceCollection services, HostBuilderContext hostBuilderContext)
+        // ConfigureServices(HostBuilderContext hostBuilderContext, IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services, HostBuilderContext hostBuilderContext)
+        {
+            // get configuration by hostBuilderContext.Configuration
+            // eg: hostBuilderContext.Configuration["Environment"]
+            
+            //services.AddSingleton<CustomService>();
+        }
+
+        // Initialize logic, executed before running test cases
+        public void Configure(IServiceProvider applicationServices)
+        {
+        }
+    }
+}
