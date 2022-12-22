595b62b4fb251a1e0d63fa8bbb96b620545ab95f
Josh Keegan <josh.keegan@gmx.com>
2022-12-22 15:10:18 +0000

    File scoped namespace

--- Xunit.DependencyInjection.xRetry/RetryTheoryDiscoveryAtRuntimeRunnerWrapper.cs
+++ Xunit.DependencyInjection.xRetry/RetryTheoryDiscoveryAtRuntimeRunnerWrapper.cs
@@ -2,27 +2,26 @@
 using Xunit.Abstractions;
 using Xunit.Sdk;
 
-namespace Xunit.DependencyInjection.xRetry
+namespace Xunit.DependencyInjection.xRetry;
+
+public class RetryTheoryDiscoveryAtRuntimeRunnerWrapper : DependencyInjectionTheoryTestCaseRunnerWrapper
 {
-    public class RetryTheoryDiscoveryAtRuntimeRunnerWrapper : DependencyInjectionTheoryTestCaseRunnerWrapper
+    /// <inheritdoc />
+    public override Type TestCaseType => typeof(RetryTheoryDiscoveryAtRuntimeCase);
+
+    /// <inheritdoc />
+    public override Task<RunSummary> RunAsync(IXunitTestCase testCase, IServiceProvider provider, IMessageSink diagnosticMessageSink,
+        IMessageBus messageBus, object?[] constructorArguments, ExceptionAggregator aggregator,
+        CancellationTokenSource cancellationTokenSource)
     {
-        /// <inheritdoc />
-        public override Type TestCaseType => typeof(RetryTheoryDiscoveryAtRuntimeCase);
-
-        /// <inheritdoc />
-        public override Task<RunSummary> RunAsync(IXunitTestCase testCase, IServiceProvider provider, IMessageSink diagnosticMessageSink,
-            IMessageBus messageBus, object?[] constructorArguments, ExceptionAggregator aggregator,
-            CancellationTokenSource cancellationTokenSource)
+        if (testCase is not IRetryableTestCase retryableTestCase)
         {
-            if (testCase is not IRetryableTestCase retryableTestCase)
-            {
-                throw new ArgumentException("Must be a retryable test case", nameof(testCase));
-            }
-
-            return RetryTestCaseRunner.RunAsync(retryableTestCase, diagnosticMessageSink, messageBus,
-                cancellationTokenSource,
-                blockingMessageBus => base.RunAsync(retryableTestCase, provider, diagnosticMessageSink,
-                    blockingMessageBus, constructorArguments, aggregator, cancellationTokenSource));
+            throw new ArgumentException("Must be a retryable test case", nameof(testCase));
         }
+
+        return RetryTestCaseRunner.RunAsync(retryableTestCase, diagnosticMessageSink, messageBus,
+            cancellationTokenSource,
+            blockingMessageBus => base.RunAsync(retryableTestCase, provider, diagnosticMessageSink,
+                blockingMessageBus, constructorArguments, aggregator, cancellationTokenSource));
     }
 }
