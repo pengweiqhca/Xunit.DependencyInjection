a5e9e90c20b0f2f481a1b8f35481b7bf8ef75b6e
pengweiqhca <pengweiqhca@sina.com>
2023-05-18 13:35:21 +0800

    Use MartinCostello.Logging.XUnit replace inbuild Logger.

---
+++ src/Xunit.DependencyInjection.Logging/LoggingBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using MartinCostello.Logging.XUnit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Xunit.DependencyInjection.Logging;
+
+public static class LoggingBuilderExtensions
+{
+    public static ILoggingBuilder AddXunitOutput(this ILoggingBuilder builder)
+    {
+        builder.Services.TryAddSingleton<TestOutputHelperAccessorWrapper>();
+
+        builder.Services.AddSingleton<ILoggerProvider>(provider => new XUnitLoggerProvider(
+            provider.GetRequiredService<TestOutputHelperAccessorWrapper>(),
+            provider.GetRequiredService<IOptions<XUnitLoggerOptions>>().Value));
+
+        return builder;
+    }
+
+    public static ILoggingBuilder AddXunitOutput(this ILoggingBuilder builder,
+        Action<XUnitLoggerOptions> configureOptions)
+    {
+        builder.Services.Configure(configureOptions);
+
+        return builder.AddXunitOutput();
+    }
+}
