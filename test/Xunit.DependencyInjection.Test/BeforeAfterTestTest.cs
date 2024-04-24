613facd493ab528edf0e2620a23161c87a3a281f
pengweiqhca <pengweiqhca@qq.com>
2024-04-24 13:06:46 +0800

    Test After

--- test/Xunit.DependencyInjection.Test/BeforeAfterTestTest.cs
+++ test/Xunit.DependencyInjection.Test/BeforeAfterTestTest.cs
@@ -2,12 +2,14 @@
 
 namespace Xunit.DependencyInjection.Test;
 
-public class BeforeAfterTestTest
+public class BeforeAfterTestTest : IDisposable
 {
     public IDependency? Dependency { get; set; }
 
     [Fact]
     public void Test() => Assert.NotNull(Dependency);
+
+    public void Dispose() => Assert.Null(Dependency);
 }
 
 public class TestBeforeAfterTest(IDependency dependency) : BeforeAfterTest
@@ -16,5 +18,11 @@ public class TestBeforeAfterTest(IDependency dependency) : BeforeAfterTest
     {
         if (testClassInstance is BeforeAfterTestTest beforeAfterTestTest)
             beforeAfterTestTest.Dependency = dependency;
+    }
+
+    public override void After(object? testClassInstance, MethodInfo methodUnderTest)
+    {
+        if (testClassInstance is BeforeAfterTestTest beforeAfterTestTest)
+            beforeAfterTestTest.Dependency = null;
     }
 }
