babef17349b7bf0672167410bba53415fc79d496
pengweiqhca <pengweiqhca@qq.com>
2025-07-16 12:00:07 +0800

    Add extensions

---
+++ src/Xunit.DependencyInjection.AspNetCoreTesting/HttpClientBuilderExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Xunit.DependencyInjection.AspNetCoreTesting;
+
+public static class HttpClientBuilderExtensions
+{
+    public static IHttpClientBuilder UseTestHandler(this IHttpClientBuilder builder) =>
+        builder.ConfigurePrimaryHttpMessageHandler(static provider =>
+            provider.GetRequiredService<ITestHttpMessageHandlerFactory>().CreateHandler());
+}
