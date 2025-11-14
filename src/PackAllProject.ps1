20b74d6341b6ee1b5cab1046ea19839036b8ef59
pengweiqhca <pengweiqhca@qq.com>
2025-11-14 10:33:53 +0800

    Support vs2026+

--- src/PackAllProject.ps1
+++ src/PackAllProject.ps1
@@ -1,15 +1,30 @@
 function FindMSBuild () {
-    if ($null -eq $env:OS) {
-        try {
-            return (Get-Command msbuild).Source;
-        }
-        catch {
-            return $null;
+    $cmd = Get-Command msbuild -type Application -ErrorAction SilentlyContinue | Select-Object -First 1;
+    if ($null -ne $cmd) {
+        return $cmd.Source;
+    }
+
+    if ($PSVersionTable.PSVersion.Major -ge 6 -and -not $IsWindows) {
+        return $null;
+    }
+
+    if ([Environment]::Is64BitProcess) {
+        $vsv = "$Env:ProgramFiles (x86)\Microsoft Visual Studio\Installer\vswhere.exe"
+    }
+    else {
+        $vsv = "$Env:ProgramFiles\Microsoft Visual Studio\Installer\vswhere.exe"
+    }
+
+    if (Test-Path $vsv) {
+        $vsv = & $vsv -all -requires Microsoft.Component.MSBuild -format json | ConvertFrom-Json | Sort-Object { [version]$_.installationVersion } -Descending | Select-Object -First 1
+
+        if (($null -ne $vsv) -and (Test-Path ($vsv.installationPath + "\MSBuild\Current\Bin\amd64\MSBuild.exe"))) {
+            return $vsv.installationPath + "\MSBuild\Current\Bin\amd64\MSBuild.exe";
         }
     }
 
     foreach ($program in ("C:\Program Files", "D:\Program Files")) {
-        foreach ($vs in (Get-ChildItem ($program + "\Microsoft Visual Studio"))) {
+        foreach ($vs in (Get-ChildItem ($program + "\Microsoft Visual Studio") | Sort-Object { $_.Name.Length -gt 2 }, @{ Expression = 'Name'; Descending = $true })) {
             foreach ($vsv in (Get-ChildItem $vs.FullName)) {
                 if (Test-Path ($vsv.FullName + "\MSBuild\Current\Bin\amd64\MSBuild.exe")) {
                     return $vsv.FullName + "\MSBuild\Current\Bin\amd64\MSBuild.exe";
@@ -37,6 +52,7 @@ if ($null -eq $MSBuild) {
     dotnet build -c Release /p:IsPacking=true ..
 }
 else {
+    Write-Host $MSBuild
     & "$MSBuild" /r /m /v:m /p:Configuration=Release /p:IsPacking=true ..
 }
 
