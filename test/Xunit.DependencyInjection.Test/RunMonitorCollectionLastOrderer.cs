fe8271f4d6eda78fd0dca90b341d360651f352e8
pengweiqhca <pengweiqhca@qq.com>
2026-08-14 14:54:45 +0800

    Use collection expressions.

--- test/Xunit.DependencyInjection.Test/RunMonitorCollectionLastOrderer.cs
+++ test/Xunit.DependencyInjection.Test/RunMonitorCollectionLastOrderer.cs
@@ -1,9 +1,12 @@
-using Xunit.v3;
+﻿using Xunit.v3;
 
 namespace Xunit.DependencyInjection.Test;
 
 public class RunMonitorCollectionLastOrderer : ITestCollectionOrderer
 {
     public IReadOnlyCollection<TTestCollection> OrderTestCollections<TTestCollection>(IReadOnlyCollection<TTestCollection> testCollections) where TTestCollection : ITestCollection =>
-        testCollections.OrderBy(c => c.TestCollectionDisplayName.StartsWith("Monitor") || c.TestCollectionDisplayName.Contains(".Monitor")).ToArray();
+    [
+        .. testCollections.OrderBy(c =>
+            c.TestCollectionDisplayName.StartsWith("Monitor") || c.TestCollectionDisplayName.Contains(".Monitor"))
+    ];
 }
--- test/Xunit.DependencyInjection.Test/TestCaseByMethodNameOrderer.cs
+++ test/Xunit.DependencyInjection.Test/TestCaseByMethodNameOrderer.cs
@@ -5,5 +5,5 @@ namespace Xunit.DependencyInjection.Test;
 public class TestCaseByMethodNameOrderer : ITestCaseOrderer
 {
     public IReadOnlyCollection<TTestCase> OrderTestCases<TTestCase>(IReadOnlyCollection<TTestCase> testCases) where TTestCase : notnull, ITestCase =>
-        testCases.OrderBy(t => t.TestMethod?.MethodName).ToArray();
+        [.. testCases.OrderBy(t => t.TestMethod?.MethodName)];
 }
